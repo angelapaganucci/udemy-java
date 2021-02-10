9a6ce8e946b3dc42b18c70a9f78ee48b48085681
angelapaganucci <angelapaganucci@hotmail.com>
2021-02-10 13:28:34 -0300

    Exercicios para iniciantes na linguagem JAVA

---
+++ src/Uri1003.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+
+public class Uri1003 {
+
+    public static void main(String[] args)  {
+
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int total;
+
+        total = A+B;
+
+        System.out.println("SOMA = "+total);
+
+        sc.close();
+
+    }
+
+}
