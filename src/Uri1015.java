e54f4a137cfdd48e7f5297ff50ef5f17460e330b
angelapaganucci <angelapaganucci@hotmail.com>
2021-02-10 13:46:46 -0300

    Exercicios para iniciantes na linguagem JAVA

---
+++ src/Uri1015.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+
+public class Uri1015 {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        double x1 = sc.nextDouble();
+        double y1 = sc.nextDouble();
+        double x2 = sc.nextDouble();
+        double y2 = sc.nextDouble();
+        double distancia;
+
+        distancia = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+
+        System.out.printf("%.4f%n",distancia);
+
+        sc.close();
+
+    }
+
+}
